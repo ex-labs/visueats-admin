dd51e6c93c88f55eb9efe27298cbb80d42bb0e0d
Justin Ritchie <justinritchie@JustinsMacmini.hsd1.fl.comcast.net>
2020-10-20 00:46:01 -0400

    view updates

--- src/views/AppRoles.vue
+++ src/views/AppRoles.vue
@@ -7,14 +7,15 @@
         <div class="inner-heading w-100">
           <h3 class="tl">Assign a New Role</h3>
         </div><!-- /heading -->
-        <div class="add-admin tl">
+        <div class="add-admin tl pb4">
           <input type="text" class="w-60 pa3 dib pa3" placeholder="Enter an Email Address">
           <select class="w-20 dib pa3 bn">
             <option>Admin</option>
             <option>Editor</option>
           </select>
         </div>
-        <div class="admin_list">
+        <div class="admin_list bt pv3">
+          <p class="i">Fetch and loop through Admin Array</p>
           <div class="admin-item">
           </div>
         </div>
--- src/views/DesignStudio.vue
+++ src/views/DesignStudio.vue
@@ -3,12 +3,9 @@
     <sidebar role="Demo Admin"></sidebar>
     <div class="main-content fl w-80 pa2 vh-100 bg-light-gray">
       <p>This is where we <span class="fw6">Customizable Styles</span>. Authentication Required.</p>
-      <div class="inner-content mw8 center pt4 flex">
-        <div class="w-50 pl4 pr2">
-          Col 1
-        </div><!-- /col -->
-        <div class="w-50 pl2 pr4">
-          Col 2
+      <div class="inner-content mw8 center pt4 flex bg-white">
+        <div class="w-100 tc pv4">
+          <p class="f2">Updating...</p>
         </div><!-- /col -->
       </div>
     </div>
